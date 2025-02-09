a88268cc45491f16975879b78719591abe4a4610
Gregory M Reis <greis003@fiu.edu>
2025-02-09 15:29:56 -0500

    Updating chapters

--- chapter2.py
+++ chapter2.py
@@ -1,48 +1,125 @@
-"""
-Activities for Chapter 2 - Data Science Tools
-The questions can be found in Canvas under:
-    Chapter 2 - Gentle Introduction to Python Programming and Data Science Tools
-"""
-
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Activity 1
+""" Example 1 """
 
-products = pd.read_csv('datasets/products.csv')
+data = {
+    'year' : [2010,2011,2012,2010,2011,2012,2010,2011,2012],
+    'team' : ['FCBarcelona','FCBarcelona','FCBarcelona','RMadrid','RMadrid','RMadrid','ValenciaCF','ValenciaCF','ValenciaCF'],
+    'wins' : [30,28,32,29,32,26,21,17,19],
+    'draws' : [6,7,4,5,4,7,8,10,8],
+    'losses' : [2,3,2,4,2,5,9,11,11]
+}
 
-print(products.head())
-print(products.tail())
-print(products.dtypes)
-print(products.shape)
-print(products.describe())
+# print(data)
 
-# Activity 2
+df = pd.DataFrame(data)
+# print(df)
 
-sales_data = pd.read_csv('datasets/sales_data.csv')
+""" Example 2 """
 
-print(sales_data.isnull().sum()) # Missing values
+edu = pd.read_csv("educ_figdp_1_Data.csv",
+                  na_values=":",
+                  usecols=["TIME","GEO","Value"])
+# print(edu)
+# print(edu.head())
+# print(edu.tail())
 
-sales_data['Sales'] = sales_data['Sales'].fillna(sales_data['Sales'].mean())
+print(edu["Value"])
+print(edu[10:14])
+print(edu.loc[90:94,["TIME","GEO"]])
 
-sales_data['Date'] = pd.to_datetime(sales_data['Date'])
+print(edu[edu["Value"] > 6.5].tail())
 
-print(sales_data) # Cleaned dataset
+print(edu[edu["Value"].isnull()].head())
 
-# Activity 3
+print(edu.max(axis=0)) # the function should be applied to the rows for each column
 
-orders = pd.read_csv('datasets/orders.csv')
+print("Pandas maximum", edu["Value"].max())
+print("Python max", max(edu["Value"]))
 
-category_sales = orders.groupby('Category')['Amount'].sum()
-print(category_sales) # Total sales by category
+s = edu["Value"]/100
+print(s.head())
 
-customer_avg = orders.groupby('Customer')['Amount'].mean()
-print(customer_avg) # Average order amount by customer
+s = edu["Value"].apply(np.sqrt)
+print(s.head())
 
-# Activity 4
+s = edu["Value"].apply(lambda d: d**2)
+print(s.head())
 
-students = pd.read_csv("datasets/students.csv")
-print(students.info())
-students["Average"] = students[["Math", "Science", "English"]].mean(axis=1)
-print(students[students["Average"]>85])
+edu["ValueNorm"] = edu["Value"]/edu["Value"].max()
+print(edu.head())
 
-print(students["Average"].rank(ascending=False))
+eduAlt = edu.drop('ValueNorm', axis=1)
+print(edu.head())
+print(eduAlt.head())
+
+edu = pd.concat([edu,
+                 pd.DataFrame({"TIME":2000,"Value":5.00,"GEO":"a"},
+                 index=[max(edu.index)+1])])
+print(edu.tail())
+
+""" Example 3 """
+
+some_data = {
+    "A":[1,None,3,None],
+    "B":[None,None,6,None],
+    "C":[7,8,9,None]
+}
+
+some_df = pd.DataFrame(some_data)
+# print(some_df)
+
+df_any = some_df.dropna(how="any")
+# print(df_any)
+
+df_all = some_df.dropna(how="all")
+print(df_all)
+
+print("Going back to example 2")
+
+eduDrop = edu.dropna(how="any", subset=["Value"])
+print(eduDrop.head())
+
+eduFilled = edu.fillna(value={"Value":0,"ValueNorm":0})
+print(eduFilled.head())
+
+edu.sort_values(by="Value",ascending=True,inplace=True)
+print(edu.head())
+
+edu.sort_index(axis=0,ascending=True,inplace=True)
+print(edu.head())
+
+group = edu[["GEO","Value"]].groupby("GEO").mean()
+print(group)
+
+filtered_edu = edu[edu["TIME"]>2005]
+
+pivedu = pd.pivot_table(filtered_edu,
+                        values="Value",
+                        index = ["GEO"],
+                        columns = ["TIME"])
+
+print(pivedu)
+
+print(pivedu.loc[["Spain","Portugal"],[2006,2011]])
+
+pivedu = pivedu.drop([
+    "Euro area (13 countries)",
+    "Euro area (15 countries)",
+    "Euro area (17 countries)",
+    "Euro area (18 countries)",
+    "European Union (25 countries)",
+    "European Union (27 countries)",
+    "European Union (28 countries)",
+], axis=0)
+
+pivedu = pivedu.rename(index={"Germany (until 1990 former territory of the FRG)":"Germany"})
+
+print(pivedu.head(15))
+pivedu = pivedu.dropna()
+print(pivedu.rank(ascending=False,method="first"))
+
+sample_Data = pd.Series([50,30,20,50,40])
+print(sample_Data.rank(method="first"))
